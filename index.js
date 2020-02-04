b37ffadf16633df3635841573b956bf07ccb1454
ayham <ayham.alaaeddin@gmail.com>
2020-02-04 16:28:03 +0100

    fertig

--- index.js
+++ index.js
@@ -15,23 +15,34 @@
 
 
 
-var zlib = require('zlib');
-var fs = require('fs');
+const zlib = require('zlib');
+const fs = require('fs');
 const path = require('path')
 
 
 
 const args = process.argv.slice(2)
+
+if(args.length == 1){
 const sourcePath = path.resolve(args[0])
 const targetFileName =path.parse(sourcePath).dir + '/' + path.parse(sourcePath).base + '.gz'
-
 var zip = zlib.createGzip();
-
-
-
 
 var read = fs.createReadStream(sourcePath);
 var write = fs.createWriteStream(targetFileName);
 
 read.pipe(zip).pipe(write);	
 console.log("Zipped Successfully");		
+
+}else{
+let sourcePath = 'input.txt'
+
+const targetFileName ='input.txt.gz'
+var zip = zlib.createGzip();
+
+var read = fs.createReadStream(sourcePath);
+var write = fs.createWriteStream(targetFileName);
+
+read.pipe(zip).pipe(write);	
+console.log("Zipped Successfully");		
+}
